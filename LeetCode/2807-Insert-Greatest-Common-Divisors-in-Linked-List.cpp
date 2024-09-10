8ece146b8b9a860123170f60b8f64e5c3478617d
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2024-09-10 05:36:03 +0530

    [2807] [Time Beats: 84.94%] [Memory Beats: 82.75%] - LeetPush

---
+++ LeetCode/2807-Insert-Greatest-Common-Divisors-in-Linked-List.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int gcd(int a, int b) {while(b){int r=a%b;a=b;b=r;}return a;}
+    ListNode* insertGreatestCommonDivisors(ListNode* son) {
+        for(ListNode* tmp=son;tmp&&tmp->next;tmp=tmp->next->next){
+            ListNode* gcdNode = new ListNode(gcd(tmp->val, tmp->next->val));
+            gcdNode->next = tmp->next;
+            tmp->next = gcdNode;}
+        return son;}};
