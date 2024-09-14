d6bd4aa7f193cfc76a0a745b4e9b90a3645be2c7
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2024-09-15 00:21:27 +0530

    [496] [Time Beats: 70.62%] [Memory Beats: 24.43%] - LeetPush

--- LeetCode/496-Next-Greater-Element-I.cpp
+++ LeetCode/496-Next-Greater-Element-I.cpp
@@ -1,16 +1,16 @@
 /*
-By   :-: savetrees
-Used :-: Basic Searching
+By     :-: savetrees
+Used :-: Map
 */
 class Solution {
 public:
-    vector<int> nextGreaterElement(vector<int>& nums1, vector<int>& nums2) {       int n = nums1.size();
-        vector<int> son(n); 
-        for (int i = 0; i < n; ++i) {
-          auto it = find(nums2.begin(), nums2.end(), nums1[i]); 
-          bool found = false;
-          for (auto j = it + 1; j != nums2.end(); ++j) {
-            if (*j > nums1[i]) {
-              son[i] = *j;
-              found = true;break;}}
-            if (!found) son[i] = -1;}return son;}};
+    vector<int> nextGreaterElement(vector<int>& n, vector<int>& m) {
+        stack<int>son;
+        unordered_map<int, int> mpp;
+        vector<int> dum(n.size());
+        for (int i = m.size() - 1; i >= 0; --i) {
+            while(!son.empty()&&son.top()<=m[i])son.pop();
+            mpp[m[i]] = son.empty()?-1:son.top();
+            son.push(m[i]);}
+        for (int i = 0; i < n.size(); ++i) dum[i] = mpp[n[i]];
+        return dum;}};
