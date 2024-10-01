def98a1d1325492448bfb8030cb7bbf7601af055
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2024-10-01 17:55:09 +0530

    [1497] [Time Beats: 96.43%] [Memory Beats: 94.58%] - LeetPush

--- LeetCode/1497-Check-If-Array-Pairs-Are-Divisible-by-k.cpp
+++ LeetCode/1497-Check-If-Array-Pairs-Are-Divisible-by-k.cpp
@@ -1,18 +1,18 @@
+/*
+By   :-: savetrees
+Used :-: Straightforward
+*/
 class Solution {
 public:
     bool canArrange(vector<int>& arr, int k) {
         vector<int>v(k,0);
         for(int &x:arr)
-        {
-            int rem=((x%k)+k)%k;
-            v[rem]++;
-        }
+        {   int rem=((x%k)+k)%k;
+            v[rem]++; }
         if(v[0]%2!=0)
         return false;
         for(int i=1;i<=k/2;i++)
-        {
-            if(v[i]!=v[k-i]) return false;
-        }
+        {    if(v[i]!=v[k-i]) return false; }
         return true;
     }
 };
