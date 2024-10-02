65b1c0ea29eaf5c881c7df53d169b3c8fa162515
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2024-10-02 09:15:38 +0530

    [1331] [Time Beats: 91.13%] [Memory Beats: 96.30%] - LeetPush

---
+++ LeetCode/1331-Rank-Transform-of-an-Array.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    vector<int> arrayRankTransform(vector<int>& arr) {
+        int n = arr.size();
+        vector<pair<int, int>> ranked(n);
+        for (int i = 0; i < n; i++)
+            ranked[i] = {arr[i], i};
+        sort(ranked.begin(), ranked.end());
+        int currRank = 0, prev = INT_MIN;
+        vector<int> result(n);
+        for (auto& p : ranked) {
+            if (p.first > prev) {
+                currRank++;
+                prev = p.first;}
+            result[p.second] = currRank;}
+        return result;}};
