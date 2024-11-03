a1ff069033d37e3c8a8d2959f050bf7487534479
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2024-11-03 07:50:31 +0530

    [160] [Time Beats: 22.42%] [Memory Beats: 64.39%] - LeetPush

---
+++ LeetCode/160-Intersection-of-Two-Linked-Lists.c
@@ -0,0 +1,11 @@
+struct ListNode *getIntersectionNode(struct ListNode *headA, struct ListNode *headB) { 
+   if (headA==NULL||headB==NULL)return NULL;
+  struct ListNode*temp1=headA; struct ListNode*temp2=headB;
+  while(temp1!=temp2)
+  {
+    if(temp1==NULL) { temp1=headB; }
+    else {temp1=temp1->next;}
+    if(temp2==NULL) { temp2=headA; }
+    else{ temp2=temp2->next; }
+  }
+  return temp1; }
