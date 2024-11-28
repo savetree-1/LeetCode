0d18f02d30aeec38dafc6bda568ed4802016ac96
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2024-11-28 17:36:31 +0530

    [134] [Time Beats: 100.00%] [Memory Beats: 10.76%] - LeetPush

--- LeetCode/134-Gas-Station.cpp
+++ LeetCode/134-Gas-Station.cpp
@@ -2,7 +2,9 @@ class Solution {
 public:
     int canCompleteCircuit(vector<int>& gas, vector<int>& cost) {
         int n = gas.size();
-        int total(0),subsum(INT_MAX),start(0);
+        int total=0;
+        int subsum=INT_MAX;
+        int start=0;
         for (int i=0;i<n;++i) {
             total += gas[i]-cost[i];
             if (total<=subsum) {
