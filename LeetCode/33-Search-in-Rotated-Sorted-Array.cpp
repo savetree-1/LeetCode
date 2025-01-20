24dbf92fbbdd73339ad0999c5d3731dfc058844b
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-01-20 16:00:10 +0530

    [33] [Time Beats: 100.00%] [Memory Beats: 30.53%] - LeetPush

--- LeetCode/33-Search-in-Rotated-Sorted-Array.cpp
+++ LeetCode/33-Search-in-Rotated-Sorted-Array.cpp
@@ -1,29 +1,32 @@
 /*
 By  :-: savetrees
-Used:-: Monotonic Stack
+Used:-: Binary Search
 */
+
 class Solution {
 public:
-    int search(vector<int>& nums,int to_find) {
-    int low=0,high=nums.size()-1;
-    while (low<=high){
-        int mid=(low+high)/2;
-        if (nums[mid]==to_find)return mid;
-        if (nums[low]<=nums[mid]) {
-            if (to_find>=nums[low]&&to_find<nums[mid]){
-                high=mid-1;
-            } else {
-                low=mid+1;
+    int search(vector<int>& nums, int target) {
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=(low+high)/2;
+            if(nums[mid]==target){
+                return mid;
             }
-        } 
-        else {
-            if(to_find>nums[mid]&&to_find<=nums[high]){
-                low=mid+1;
-            } else{
-                high=mid-1;
+            else if(nums[low]<=nums[mid]){
+                if(target>=nums[low] && target<=nums[mid]){
+                    high=mid-1;
+                }else{
+                    low=mid+1;
+                }
             }
-        }
+            else{
+                if(target<=nums[high] && target>=nums[mid]){
+                    low=mid+1;
+                }else{
+                    high=mid-1;
+                }
+            }
+           
+        }return -1;
     }
-    return -1;
-}
 };
