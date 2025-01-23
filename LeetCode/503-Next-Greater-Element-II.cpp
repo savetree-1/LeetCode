a80922e61a9a686f720a2839d0acdfd1ce520b8f
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-01-23 19:18:35 +0530

    [503] [Time Beats: 49.13%] [Memory Beats: 37.45%] - LeetPush

--- LeetCode/503-Next-Greater-Element-II.cpp
+++ LeetCode/503-Next-Greater-Element-II.cpp
@@ -6,8 +6,8 @@ class Solution {
 public:
     vector<int> nextGreaterElements(vector<int>& nums) {
         int n = nums.size();vector<int> son(n, -1);stack<int> st;
-        for (int i = 2 * n - 1; i >= 0; i--) {
-            while (!st.empty() && st.top() <= nums[i % n]) st.pop();
-            if (i < n) son[i] = !st.empty() ? st.top() : -1;
-            st.push(nums[i % n]);}
+        for (int i =2*n-1;i>=0;i--) {
+            while (!st.empty() && st.top()<=nums[i%n]) st.pop();
+            if (i < n) son[i]=!st.empty()?st.top():-1;
+            st.push(nums[i%n]);}
         return son;}};
