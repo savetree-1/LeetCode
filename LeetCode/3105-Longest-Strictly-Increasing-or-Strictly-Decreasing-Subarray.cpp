4b201bf4bec4df5694beacf1340eb42848c06d2f
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-02-03 10:27:28 +0530

    [3105] [Time Beats: 100.00%] [Memory Beats: 99.12%] - LeetPush

--- LeetCode/3105-Longest-Strictly-Increasing-or-Strictly-Decreasing-Subarray.cpp
+++ LeetCode/3105-Longest-Strictly-Increasing-or-Strictly-Decreasing-Subarray.cpp
@@ -7,30 +7,27 @@ public:
     int longestMonotonicSubarray(vector<int>& nums) {
         int inc=1;
         int maxi=1;
-        int maxi2=1;
         int dec=1;
         for(int i=1;i<nums.size();i++){
             if(nums[i]>nums[i-1])
             {
                 inc++;
+                dec=1;
                 maxi=max(maxi,inc);
+            }
+            else if (nums[i]<nums[i-1])
+            {
+                dec++;
+                inc=1;
+                maxi=max(maxi,dec);
             }
             else
             {
                 inc=1;
-            }
-        }
-        for(int i=1;i<nums.size();i++){
-            if(nums[i]<nums[i-1])
-            {
-                dec++;
-                maxi2=max(maxi2,dec);
-            }
-            else
-            {
                 dec=1;
             }
         }
-        return max(maxi,maxi2);
+        
+        return maxi;
     }
 };
