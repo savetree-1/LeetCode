565fd27fec2ac684802261793afad4e6e975e69d
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-02-23 15:20:21 +0530

    [50] [Time Beats: 100.00%] [Memory Beats: 44.75%] - LeetPush

---
+++ LeetCode/50-Pow(x,-n).cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    double myPow(double x, int n) {
+        long long k=n;
+        if(k<0)k=-k;
+        double res=1.0;
+        while(k)
+        {
+            if(k%2==1)res*=x;
+            x*=x;
+            k/=2;
+        }
+        if(n>0)return res;
+        return 1/res;
+    }
+};
