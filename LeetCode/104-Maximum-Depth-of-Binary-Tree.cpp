72b04bef745dbba8f84b83d4bf66987cd30c8fb8
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-02-24 09:02:25 +0530

    [104] [Time Beats: 100.00%] [Memory Beats: 44.67%] - LeetPush

---
+++ LeetCode/104-Maximum-Depth-of-Binary-Tree.cpp
@@ -0,0 +1,11 @@
+/*
+By   :: savetrees
+Used :: Basic Recursion
+*/
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        if(root==NULL)return 0;
+        return 1+max(maxDepth(root->left),maxDepth(root->right));   
+    }
+};
