a8f42e198b4078f3a7ac237e93deea60b7d2f26c
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-01 06:30:31 +0530

    Time: 0 ms (100.00%) | Memory: 13.2 MB (62.06%) - LeetSync

--- LeetCode/2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
+++ LeetCode/2551-apply-operations-to-an-array/apply-operations-to-an-array.cpp
@@ -1,18 +1,18 @@
 class Solution {
 public:
     vector<int> applyOperations(vector<int>& nums) {
-        int n = nums.size();
-        for (int i = 0; i < n - 1; i++) {
-            if (nums[i] == nums[i + 1]) {
-                nums[i] *= 2;
-                nums[i + 1] = 0;
+        int n=nums.size();
+        for (int i=0;i<n-1;i++) {
+            if (nums[i]==nums[i+1]) {
+                nums[i]*=2;
+                nums[i+1]=0;
             }
         }
         vector<int> result;
-        for (int i = 0; i < n; i++) {
-            if (nums[i] != 0) result.push_back(nums[i]);
+        for (int i=0;i<n;i++) {
+            if (nums[i]!=0) result.push_back(nums[i]);
         }
-        while (result.size() < n) {
+        while (result.size()<n) {
             result.push_back(0);
         }
         return result;
