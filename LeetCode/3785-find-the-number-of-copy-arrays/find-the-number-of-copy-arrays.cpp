455fe275bee56660ba2688f0f15e44f805d7a179
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-02 00:34:03 +0530

    Time: 0 ms (100.00%) | Memory: 181.9 MB (0.00%) - LeetSync

---
+++ LeetCode/3785-find-the-number-of-copy-arrays/find-the-number-of-copy-arrays.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int getValidRange(vector<int>& original, vector<vector<int>>& bounds) {
+        int n = original.size();
+        int minRange = bounds[0][0];
+        int maxRange = bounds[0][1];
+        for (int i = 1; i < n; ++i) {
+            int shift = original[i] - original[0];
+            minRange = max(minRange, bounds[i][0] - shift);
+            maxRange = min(maxRange, bounds[i][1] - shift);
+            if (minRange > maxRange) return 0;
+        }
+        return maxRange - minRange + 1;
+    }
+    int countArrays(vector<int>& original, vector<vector<int>>& bounds) {
+        return getValidRange(original, bounds);
+    }
+};
