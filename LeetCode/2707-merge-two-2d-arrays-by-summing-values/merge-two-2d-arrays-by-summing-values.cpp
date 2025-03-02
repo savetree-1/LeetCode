cece135370e6e1c0c195127616db6dbe415cf251
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-02 19:39:06 +0530

    Time: 1 ms (45.39%) | Memory: 15.5 MB (7.08%) - LeetSync

---
+++ LeetCode/2707-merge-two-2d-arrays-by-summing-values/merge-two-2d-arrays-by-summing-values.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    vector<vector<int>> mergeArrays(vector<vector<int>>& nums1, vector<vector<int>>& nums2) {
+        map<int,int>mpp;
+        for(int i=0;i<nums1.size();i++)
+        {
+            mpp[nums1[i][0]] += nums1[i][1];
+        }
+        for(int i=0;i<nums2.size();i++)
+        {
+            mpp[nums2[i][0]] += nums2[i][1];
+        }
+        vector<vector<int>>arr;
+        for(auto pair:mpp)
+        {
+            vector<int>dummy;
+            dummy.push_back(pair.first);
+            dummy.push_back(pair.second);
+            arr.push_back(dummy);
+        }
+        return arr;
+    }
+};
