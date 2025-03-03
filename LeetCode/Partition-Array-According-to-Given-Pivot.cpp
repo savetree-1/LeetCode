b8d19891d5c3605ad3f6d755f5903f9379881b2a
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-03 08:36:26 +0530

    [2161] [Time Beats: 100.00%] [Memory Beats: 77.25%] - LeetPush

--- LeetCode/Partition-Array-According-to-Given-Pivot.cpp
+++ LeetCode/Partition-Array-According-to-Given-Pivot.cpp
@@ -1,33 +1,21 @@
 /*
 By   :: savetrees
-Used :: In Place Modification using Erase and Insert
+Used :: Optimal using Extra Space
 */
 class Solution {
 public:
-    vector<int> pivotArray(vector<int>& nums,int pivot) {
-        int ptr=0;
-        bool check=false;
-        for (int i=0;i<nums.size();i++) {
-            int n=nums[i];
-            if (nums[i]<pivot) {  
-                if (ptr!=i) {
-                    int temp=nums[i];
-                    nums.erase(nums.begin()+i);
-                    nums.insert(nums.begin()+ptr,temp);
-                }
-                ptr++;
-            }
+    vector<int> pivotArray(vector<int>& nums, int pivot) {
+        vector<int> dummy(nums.size());
+        int left=0,right=nums.size()-1,cnt=0;
+        for (int num:nums) {
+            if (num<pivot)dummy[left++]=num;
+            else if(num==pivot)cnt++;
         }
-        for (int i=ptr;i<nums.size();i++) {  
-            if (nums[i]==pivot) {
-                if (ptr!=i) {
-                    int temp=nums[i];
-                    nums.erase(nums.begin()+i);
-                    nums.insert(nums.begin()+ptr,temp);
-                }
-                ptr++;
-            }
+        int mid=left;
+        while(cnt--)dummy[left++]=pivot;
+        for (int num:nums) {
+            if(num>pivot)dummy[left++]=num;
         }
-        return nums;
+        return dummy;
     }
 };
