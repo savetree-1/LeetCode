a8146b98a2951ad907c4204231ce205f23fe7736
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-04 21:44:05 +0530

    [1011] [Time Beats: 92.65%] [Memory Beats: 35.57%] - LeetPush

---
+++ LeetCode/Capacity-To-Ship-Packages-Within-D-Days.cpp
@@ -0,0 +1,34 @@
+/*
+By   :: savetrees
+Used :: Binary Search
+*/
+class Solution {
+public:
+    int isValid(int check,vector<int>& weights,int days) {
+        int count=1,pre=0;
+        for (int num : weights) {
+            pre+=num;
+            if (pre>check) {
+                count++;
+                pre=num;
+            }
+        }
+        return count;
+    }
+    
+    int shipWithinDays(vector<int>& weights,int days) {
+        int maxi=0,sum=0;
+        for (int num:weights) {
+            maxi=max(maxi,num);
+            sum+=num;
+        }
+        int low=maxi,high=sum;
+        while (low<=high) {
+            int mid=low+(high-low)/2;
+            int dummy=isValid(mid,weights,days);
+            if (dummy>days)low=mid+1;
+            else high=mid-1;
+        }
+        return low;
+    }
+};
