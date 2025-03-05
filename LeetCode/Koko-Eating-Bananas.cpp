da7630f2df2fb2850433c85d65b322454eda3826
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-05 21:43:28 +0530

    [875] [Time Beats: 95.86%] [Memory Beats: 74.75%] - LeetPush

---
+++ LeetCode/Koko-Eating-Bananas.cpp
@@ -0,0 +1,26 @@
+/*
+By   :: savetrees
+Used :: Binary Search
+*/
+typedef long long ll;
+class Solution {
+public:
+    ll check(vector<int>&piles,int h,int mini)
+    {
+        ll cnt=0;
+        for(int num:piles)cnt+=(num+mini-1)/mini;
+        return cnt;
+    }
+    int minEatingSpeed(vector<int>& piles, int h) {
+        int maxi=*max_element(piles.begin(),piles.end());
+        int mini=1;
+        int low=mini;
+        int high=maxi;
+        while(low<=high){
+        int mid=low+(high-low)/2;
+        if (check(piles,h,mid)<=h)high=mid-1; 
+        else low=mid+1;
+    }
+        return low;
+    }
+};
