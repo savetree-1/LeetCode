fd46bca118d51a062de8c1077500ee7f34a0a310
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-06 06:26:56 +0530

    Time: 0 ms (100.00%) | Memory: 25.6 MB (95.09%) - LeetSync

--- LeetCode/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
+++ LeetCode/3227-find-missing-and-repeated-values/find-missing-and-repeated-values.cpp
@@ -1,9 +1,27 @@
-class Solution{public:vector<int> findMissingAndRepeatedValues(vector<vector<int>>&grid){ 
-    int n=grid.size(); 
-    long long S=0,P=0,S_actual=0,P_actual=0; 
-    for(int i=1;i<=n*n;i++){S+=i;P+=(long long)i*i;} 
-    for(auto&row:grid)for(int num:row){S_actual+=num;P_actual+=(long long)num*num;} 
-    long long D=S_actual-S,Q=P_actual-P,S2=Q/D; 
-    int repeated=(D+S2)/2,missing=S2-repeated; 
-    return{repeated,missing}; 
-}};
+/*
+By   :: savetrees
+Used :: Maths
+*/
+typedef long long ll;
+class Solution {
+public:
+    vector<int> findMissingAndRepeatedValues(vector<vector<int>>& grid) {
+        int size=grid.size();
+        ll expectedSum=0,expectedSquareSum=0,actualSum=0,actualSquareSum = 0;
+        for(int i=1;i<=size*size;i++) {
+            expectedSum+=i;
+            expectedSquareSum+=(long long)i*i;
+        }
+        for(auto& row:grid)
+            for(int num:row) {
+                actualSum+=num;
+                actualSquareSum+=(long long)num*num;
+            }
+        long long diffSum=actualSum-expectedSum,
+                  diffSquareSum=actualSquareSum-expectedSquareSum,
+                  sumValues=diffSquareSum/diffSum;
+        int repeatedValue=(diffSum+sumValues)/2,
+            missingValue=sumValues-repeatedValue;
+        return {repeatedValue, missingValue};
+    }
+};
