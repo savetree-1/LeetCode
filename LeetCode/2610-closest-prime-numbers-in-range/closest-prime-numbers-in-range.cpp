648e7741ea80c546b13bfa2c5947b86467202095
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-07 08:24:00 +0530

    Time: 187 ms (50.92%) | Memory: 23.3 MB (42.04%) - LeetSync

--- LeetCode/2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
+++ LeetCode/2610-closest-prime-numbers-in-range/closest-prime-numbers-in-range.cpp
@@ -1,32 +1,35 @@
 /*
 By   :: savetrees
-Used :: Brute Force
+Used :: Optimal (Sieve of E. )
 */
 class Solution {
 public:
-    bool isPrime(int num) {
-        if(num<2)return false;
-        for(int i=2;i*i<=num;i++) {
-            if(num%i==0)return false;
-        }
-        return true;
-    }
     vector<int> closestPrimes(int left, int right) {
-        vector<int>record;
-        if(left==2||left==1)record.push_back(2);
-        if(left%2==0)left++;
-        for(int i=left;i<=right;i+=2) {
-            if(isPrime(i))record.push_back(i);
-        }
-        if(record.size()<2)return{-1,-1};
-        int min_diff=INT_MAX,a,b;
-        for(int i=1;i<record.size();i++) {
-            if(record[i]-record[i-1]<min_diff) {
-                a=record[i-1];
-                b=record[i];
-                min_diff=record[i]-record[i-1];
+        vector<bool> isPrime(right + 1, true);
+        isPrime[0] = isPrime[1] = false;
+        for(int i = 2; i * i <= right; i++) {
+            if(isPrime[i]) {
+                for(int j = i * i; j <= right; j += i) {
+                    isPrime[j] = false;
+                }
             }
         }
-        return{a,b};
+
+        vector<int> record;
+        for(int i = left; i <= right; i++) {
+            if(isPrime[i]) record.push_back(i);
+        }
+
+        if(record.size() < 2) return {-1, -1};
+
+        int min_diff = INT_MAX, a = -1, b = -1;
+        for(int i = 1; i < record.size(); i++) {
+            if(record[i] - record[i - 1] < min_diff) {
+                a = record[i - 1];
+                b = record[i];
+                min_diff = record[i] - record[i - 1];
+            }
+        }
+        return {a, b};
     }
 };
