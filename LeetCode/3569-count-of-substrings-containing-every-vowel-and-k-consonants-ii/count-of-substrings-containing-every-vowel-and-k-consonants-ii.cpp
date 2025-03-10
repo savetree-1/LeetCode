6ac680c60bea22dd29a7ebcf3589e1e4add4758f
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-10 10:19:47 +0530

    Time: 415 ms (21.09%) | Memory: 68.1 MB (11.78%) - LeetSync

---
+++ LeetCode/3569-count-of-substrings-containing-every-vowel-and-k-consonants-ii/count-of-substrings-containing-every-vowel-and-k-consonants-ii.cpp
@@ -0,0 +1,28 @@
+class Solution {
+public:
+    long long countOfSubstrings(string word, int k) {
+        long long ans=0;
+        int n=word.length();
+        set<char>vowel={'a','e','i','o','u'};
+        vector<int>cons;
+        for(int i=0;i<n;i++)if(vowel.find(word[i])==vowel.end())cons.push_back(i);
+        int i=0,j=0,c=0;
+        map<int,int>mp;
+        while(i<n){
+            while(j<n &&( mp.size()<5 || c<k)){
+                if(vowel.find(word[j])!=vowel.end())mp[word[j]]++;
+                else c++;    
+                j++;
+            }
+            if(mp.size()==5 && c==k){
+                int nxt=upper_bound(cons.begin(),cons.end(),j-1)-cons.begin();
+                if(nxt==cons.size())nxt=n;
+                else nxt=cons[nxt];    
+                ans+=nxt-j+1;
+            }
+            if(vowel.find(word[i])==vowel.end())c--;
+            else{
+                mp[word[i]]--;
+                if(mp[word[i]]==0)mp.erase(word[i]);}
+            i++;}
+        return ans;}};
