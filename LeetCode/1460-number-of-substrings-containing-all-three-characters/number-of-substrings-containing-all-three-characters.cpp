e140b39375839dc821137db74ad44dcb9f37c187
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-11 08:19:21 +0530

    Time: 5 ms (76.93%) | Memory: 10.8 MB (94.17%) - LeetSync

--- LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -5,20 +5,14 @@ Used :: Sliding Window
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int low=0,high=0,a=0,b=0,c=0,cnt=0;
-        while(high<s.length()){
-            if(s[high]=='a')a++;
-            else if(s[high]=='b')b++;
-            else c++;
-            while(a>=1 && b>=1 && c>=1){
-                cnt += s.length()-high;
-                if(s[low]=='a')a--;
-                else if(s[low]=='b')b--;
-                else c--;
-                low++;
+        int count[3]={0,0,0},low=0,res=0;
+        for (int high=0;high < s.length();high++) {
+            count[s[high]-'a']++;
+            while (count[0]&&count[1]&&count[2]) {
+                res += s.length()-high;
+                count[s[low++]-'a']--;
             }
-            high++;
         }
-        return cnt;
+        return res;
     }
 };
