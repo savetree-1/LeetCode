1a40e0f8d6bb44223c9d856cc3b29f2cd388733c
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-11 08:16:57 +0530

    Time: 3 ms (90.05%) | Memory: 11 MB (81.05%) - LeetSync

--- LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
+++ LeetCode/1460-number-of-substrings-containing-all-three-characters/number-of-substrings-containing-all-three-characters.cpp
@@ -1,13 +1,13 @@
 class Solution {
 public:
     int numberOfSubstrings(string s) {
-        int low=0, high=0, a=0, b=0, c=0, cnt=0;
+        int low=0,high=0,a=0,b=0,c=0,cnt=0;
         while(high<s.length()){
             if(s[high]=='a')a++;
             else if(s[high]=='b')b++;
             else c++;
             while(a>=1 && b>=1 && c>=1){
-                cnt += s.length() - high;
+                cnt += s.length()-high;
                 if(s[low]=='a')a--;
                 else if(s[low]=='b')b--;
                 else c--;
