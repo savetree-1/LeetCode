ca522d308ea1c7f495ad2de06af7f075954ee649
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-16 12:37:49 +0530

    [2594] [Time Beats: 31.07%] [Memory Beats: 74.95%] - LeetPush

---
+++ LeetCode/Minimum-Time-to-Repair-Cars.cpp
@@ -0,0 +1,27 @@
+/*
+By   :: savetrees
+Used :: Binary Search
+*/
+typedef long long int ll;
+class Solution {
+    long long tot_cars(long long min, vector<int>& ranks){
+        ll cnt = 0;
+        for(int i = 0 ; i < ranks.size(); i++){
+            cnt += sqrt(min/ranks[i]);
+        }
+        return cnt;
+    }
+public:
+    long long repairCars(vector<int>& ranks, int cars) {
+        ll low = 1;
+        ll high = *max_element(ranks.begin(), ranks.end()) * 1LL * cars * cars;
+        while (low <= high) {
+            ll mid = (low + high) / 2;
+            if (tot_cars(mid, ranks) >= cars)
+                high = mid - 1;
+            else
+                low = mid + 1;
+        }
+        return low;
+    }
+};
