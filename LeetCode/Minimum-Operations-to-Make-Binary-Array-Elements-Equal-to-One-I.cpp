040f7bda236e360cd7846fff478abaf271f5c388
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-03-19 21:14:44 +0530

    [3191] [Time Beats: 90.49%] [Memory Beats: 47.55%] - LeetPush

---
+++ LeetCode/Minimum-Operations-to-Make-Binary-Array-Elements-Equal-to-One-I.cpp
@@ -0,0 +1,20 @@
+/*
+By   :: savetrees
+Used :: Brute Force
+*/
+class Solution {
+public:
+    int minOperations(vector<int>& nums) {
+        int n = nums.size();
+        int i = 0,ans = 0;
+        for (; i<n-2; i++){
+            if (nums[i] == 1) continue;
+            ans++;
+            for (int j = i; j<i+3; j++){
+                nums[j] = !nums[j];
+            }
+        }
+        if (nums[i] == 0 || nums[i+1] == 0) return -1;
+        return ans;
+    }
+};
