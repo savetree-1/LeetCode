c930c731a5a671ee8a9152bd835d8fd3e5abc1b6
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-06-27 00:17:34 +0530

    Time: 20 ms (62.39%) | Memory: 50.6 MB (30.61%) - LeetSync

--- LeetCode/208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
+++ LeetCode/208-implement-trie-prefix-tree/implement-trie-prefix-tree.cpp
@@ -1,45 +1,54 @@
-class Trie {
-    struct Node {
-        Node* links[26] = {};
-        bool isEnd = false;
+class Trie
+{
+    struct Node{
+        Node* links[26]={};
+        bool isEnd=false;
     };
-    
     Node* root;
-
 public:
-    Trie() {
-        root = new Node();
+    Trie()
+    {
+        root=new Node();    
     }
-    
-    void insert(string word) {
-        Node* node = root;
-        for(char ch : word) {
-            int idx = ch - 'a';
+    void insert(string word)
+    {
+        Node* node=root;
+        for(char ch:word)
+        {
+            int idx=ch-'a';
             if(!node->links[idx])
-                node->links[idx] = new Node();
-            node = node->links[idx];
+            {
+                node->links[idx]=new Node();
+            }
+            node=node->links[idx];
         }
-        node->isEnd = true;
+        node->isEnd=true;
     }
-    
-    bool search(string word) {
-        Node* node = root;
-        for(char ch : word) {
-            int idx = ch - 'a';
+    bool search(string word)
+    {
+        Node* node=root;
+        for(char ch:word)
+        {
+            int idx=ch-'a';
             if(!node->links[idx])
+            {
                 return false;
-            node = node->links[idx];
+            }
+            node=node->links[idx];
         }
         return node->isEnd;
     }
-    
-    bool startsWith(string prefix) {
-        Node* node = root;
-        for(char ch : prefix) {
-            int idx = ch - 'a';
+    bool startsWith(string prefix)
+    {
+        Node* node=root;
+        for(char ch:prefix)
+        {
+            int idx=ch-'a';
             if(!node->links[idx])
+            {
                 return false;
-            node = node->links[idx];
+            }
+            node=node->links[idx];
         }
         return true;
     }
