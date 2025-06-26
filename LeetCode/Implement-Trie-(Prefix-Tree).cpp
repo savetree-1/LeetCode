a87de8e3b8c4f892f31114207630ccbcef0e8319
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-06-27 00:22:58 +0530

    [208] [Time Beats: 86.90%] [Memory Beats: 30.61%] - LeetPush

---
+++ LeetCode/Implement-Trie-(Prefix-Tree).cpp
@@ -0,0 +1,60 @@
+/*
+By   :: savetree
+Used :: Trie
+*/
+
+class Trie
+{
+    struct Node{
+        Node* links[26]={};
+        bool isEnd=false;
+    };
+    Node* root;
+public:
+    Trie()
+    {
+        root=new Node();    
+    }
+    void insert(string word)
+    {
+        Node* node=root;
+        for(char ch:word)
+        {
+            int idx=ch-'a';
+            if(!node->links[idx])
+            {
+                node->links[idx]=new Node();
+            }
+            node=node->links[idx];
+        }
+        node->isEnd=true;
+    }
+    bool search(string word)
+    {
+        Node* node=root;
+        for(char ch:word)
+        {
+            int idx=ch-'a';
+            if(!node->links[idx])
+            {
+                return false;
+            }
+            node=node->links[idx];
+        }
+        return node->isEnd;
+    }
+    bool startsWith(string prefix)
+    {
+        Node* node=root;
+        for(char ch:prefix)
+        {
+            int idx=ch-'a';
+            if(!node->links[idx])
+            {
+                return false;
+            }
+            node=node->links[idx];
+        }
+        return true;
+    }
+};
