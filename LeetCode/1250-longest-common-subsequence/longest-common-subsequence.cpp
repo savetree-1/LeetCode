cb0806f85b99c99055aada9a042535f0889e49d5
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-06-26 20:46:02 +0530

    Time: 31 ms (35.82%) | Memory: 13 MB (82.28%) - LeetSync

--- LeetCode/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ LeetCode/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,19 +1,20 @@
-/*
-By    :::: savetrees
-Using :::: Memoizaton
-*/
 class Solution {
 public:
-    int dp[1001][1001];
-    int f(string&a,string&b,int i,int j){
-    if(i==0||j==0)return 0;
-    if(dp[i][j]!=-1)return dp[i][j];
-    if(a[i-1]==b[j-1])return dp[i][j]=1+f(a,b,i-1,j-1);
-    return dp[i][j]=max(f(a,b,i-1,j),f(a,b,i,j-1));
-    }
-    int longestCommonSubsequence(string a,string b){
-    memset(dp,-1,sizeof(dp));
-    return f(a,b,a.size(),b.size());
+    int dp[1001][1001]; // assuming max length of strings ≤ 1000
+
+    int lcs(string &a, string &b, int i, int j) {
+        if(i == 0 || j == 0) return 0;
+
+        if(dp[i][j] != -1) return dp[i][j];
+
+        if(a[i - 1] == b[j - 1])
+            return dp[i][j] = 1 + lcs(a, b, i - 1, j - 1);
+        else
+            return dp[i][j] = max(lcs(a, b, i - 1, j), lcs(a, b, i, j - 1));
     }
 
+    int longestCommonSubsequence(string text1, string text2) {
+        memset(dp, -1, sizeof(dp));
+        return lcs(text1, text2, text1.size(), text2.size());
+    }
 };
