49ac2f7367f27bc38304611d44d9f6c5dcebcff0
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-06-30 20:07:26 +0530

    Time: 63 ms (76.23%) | Memory: 27.8 MB (81.54%) - LeetSync

--- LeetCode/403-frog-jump/frog-jump.cpp
+++ LeetCode/403-frog-jump/frog-jump.cpp
@@ -1,20 +1,20 @@
 /*
 By   :: savetree-1
-Used :: Recursion
+Used :: Memoization
 */
 class Solution {
 public:
-    unordered_map<int, int> stonePos;
-unordered_map<string, bool> memo;
-    bool dfs(vector<int>& stones, int pos, int jump) {
+    unordered_map<int,int> stonePos;
+unordered_map<string,bool> memo;
+    bool dfs(vector<int>& stones,int pos,int jump) {
         if (pos == stones.back()) return true;
-        string key = to_string(pos) + "_" + to_string(jump);
+        string key=to_string(pos)+"_"+to_string(jump);
         if (memo.count(key)) return memo[key];
-        for (int step = jump - 1; step <= jump + 1; ++step) {
-            if (step > 0 && stonePos.count(pos + step)) {
-                if (dfs(stones, pos + step, step)) return memo[key] = true;}}
-        return memo[key] = false;}
+        for (int step=jump - 1; step<=jump+1; ++step) {
+            if (step > 0&&stonePos.count(pos+step)) {
+                if (dfs(stones,pos+step,step)) return memo[key]=true;}}
+        return memo[key]=false;}
     bool canCross(vector<int>& stones) {
-        for (int i = 0; i < stones.size(); i++) {stonePos[stones[i]] = i;}
-        return dfs(stones, 0, 0);}
+        for (int i=0; i < stones.size(); i++) {stonePos[stones[i]]=i;}
+        return dfs(stones,0,0);}
 };
