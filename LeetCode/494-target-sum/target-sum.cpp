80a5635f1638c482695ef308c3fc84d2ab303040
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-01 20:51:51 +0530

    Time: 3 ms (87.90%) | Memory: 15.7 MB (43.09%) - LeetSync

--- LeetCode/494-target-sum/target-sum.cpp
+++ LeetCode/494-target-sum/target-sum.cpp
@@ -1,22 +1,25 @@
 /*
-By   :: savetrees
-Used :: Subset sum with Target Concept / Backtracking
+By   :: 
+Used :: Memoization
 */
 typedef vector<int> vi;
+typedef vector<vi> vvi;
 class Solution {
 public:
     int findTargetSumWays(vi& nums, int target) {
         int total=accumulate(nums.begin(),nums.end(),0);
-        if((target + total) % 2 != 0 || abs(target) > total) return 0;
-        int subsetSum = (target + total) / 2;
-        return func(nums, 0, subsetSum);
+        if((target+total)%2!=0||abs(target) > total) return 0;
+        int subsetSum=(target+total)/2;
+        vvi memo(nums.size()+1,vi(subsetSum+1,-1));
+        return func(nums, 0, subsetSum,memo);
     }
-    int func(vi &arr, int index, int rem) {
-        if(index == arr.size()) return rem == 0 ? 1 : 0;
-        int inc = 0;
+    int func(vi &arr, int index, int rem,vvi &memo) {
+        if(index==arr.size())return rem==0?1:0;
+        if(memo[index][rem]!=-1)return memo[index][rem];
+        int inc=0;
         if(arr[index] <= rem)
-            inc = func(arr, index + 1, rem - arr[index]);
-        int exc = func(arr, index + 1, rem);
-        return inc + exc;
+            inc=func(arr, index + 1, rem - arr[index],memo);
+        int exc=func(arr, index + 1, rem,memo);
+        return memo[index][rem]=inc+exc;
     }
 };
