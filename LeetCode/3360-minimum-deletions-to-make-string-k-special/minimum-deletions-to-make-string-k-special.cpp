e5724ef9c8dfef0f9e3146e079dc4b1d104f44eb
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-03 10:32:56 +0530

    Time: 9 ms (54.70%) | Memory: 19.7 MB (39.80%) - LeetSync

---
+++ LeetCode/3360-minimum-deletions-to-make-string-k-special/minimum-deletions-to-make-string-k-special.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int minimumDeletions(string word, int k){
+        int freq[26]={};
+        for(char ch:word)freq[ch-'a']++;
+        vector<int>arr;
+        for(int i=0;i<26;i++)
+        {
+            if(freq[i]>0)arr.push_back(freq[i]);
+        }
+        sort(arr.begin(),arr.end());
+        int ans=INT_MAX;
+        for(int i=0;i<arr.size();i++)
+        {
+        
+            int rem=0;
+            for(int j=0;j<i;j++)rem+=arr[j];
+            for(int j=i+1;j<arr.size();j++)
+            {
+                if(arr[j]>arr[i]+k)rem+=arr[j]-(arr[i]+k);
+            }
+            ans=min(ans,rem);
+        }
+        return ans;
+    }
+
+};
