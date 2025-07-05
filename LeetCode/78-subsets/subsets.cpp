c6f56d159bfbddd76e1a7ad29e7bf0637fa95e13
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-05 17:15:38 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (60.85%) - LeetSync

--- LeetCode/78-subsets/subsets.cpp
+++ LeetCode/78-subsets/subsets.cpp
@@ -1,22 +1,21 @@
 /*
 BY    :: savetrees
-USING :: Bitmasking
+USING :: Iterative Cascading
 */
 class Solution {
 public:
     vector<vector<int>> subsets(vector<int>& nums) {
-        int n = nums.size();
-        vector<vector<int>> result;
-        int total = 1 << n;
-        for(int mask = 0; mask < total; ++mask){
-            vector<int> subset;
-            for(int j = 0; j < n; ++j){
-                if(mask & (1 << j)){
-                    subset.push_back(nums[j]);
-                }
-            }
-            result.push_back(subset);
+       vector<vector<int>>res={{}};
+       for(int num:nums)
+       {
+        int size=res.size();
+        for(int i=0;i<size;i++)
+        {
+            vector<int>sub=res[i];
+            sub.push_back(num);
+            res.push_back(sub);
         }
-        return result;
+       }
+       return res; 
     }
 };
