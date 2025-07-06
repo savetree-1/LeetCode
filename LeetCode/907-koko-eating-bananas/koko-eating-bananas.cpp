7aa368ff88b5e98fd756058e2b088e839eccee5a
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-06 20:30:43 +0530

    Time: 2 ms (96.28%) | Memory: 22.8 MB (96.41%) - LeetSync

--- LeetCode/907-koko-eating-bananas/koko-eating-bananas.cpp
+++ LeetCode/907-koko-eating-bananas/koko-eating-bananas.cpp
@@ -1,26 +1,25 @@
-/*
-By   :: savetrees
-Used :: Binary Search
-*/
-typedef long long ll;
 class Solution {
 public:
-    ll check(vector<int>&piles,int h,int mini)
-    {
-        ll cnt=0;
-        for(int num:piles)cnt+=(num+mini-1)/mini;
-        return cnt;
+    bool check(vector<int>& piles, int h, int k) {
+        if (k <= 0) return false;
+        long long hours = 0;
+        for (int num : piles)
+            hours += (num + k - 1) / k; // safer than ceil
+        return hours <= h;
     }
+
     int minEatingSpeed(vector<int>& piles, int h) {
-        int maxi=*max_element(piles.begin(),piles.end());
-        int mini=1;
-        int low=mini;
-        int high=maxi;
-        while(low<=high){
-        int mid=low+(high-low)/2;
-        if (check(piles,h,mid)<=h)high=mid-1; 
-        else low=mid+1;
-    }
-        return low;
+        int low = 1, high = *max_element(piles.begin(), piles.end());
+        int ans = high;
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+            if (check(piles, h, mid)) {
+                ans = mid;
+                high = mid - 1;
+            } else {
+                low = mid + 1;
+            }
+        }
+        return ans;
     }
 };
