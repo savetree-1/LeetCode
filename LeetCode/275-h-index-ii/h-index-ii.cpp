966d24aea5a4228c09532dc4c6cea501775f66e0
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-06 18:14:56 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (46.66%) - LeetSync

--- LeetCode/275-h-index-ii/h-index-ii.cpp
+++ LeetCode/275-h-index-ii/h-index-ii.cpp
@@ -1,16 +1,14 @@
-/*
-By   :: savetrees
-Used :: Binary Search
-*/
 class Solution {
 public:
-    int hIndex(vector<int>& ct){
-    int n = ct.size(), l = 0, r = n;
-    while(l < r){
-        int mid = (l + r) / 2;
-        if(ct[mid] >= n - mid) r = mid;
-        else l = mid + 1;
+    int hIndex(vector<int>& citations) {
+        int n=citations.size();
+        int low=0;int high=n-1;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(citations[mid]<n-mid)low=mid+1;
+            else high=mid-1;
+        }
+        return n-low;
     }
-    return n - l;
-}
 };
