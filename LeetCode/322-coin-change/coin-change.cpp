10b11fd36bd8e98a2af0b2e1c74739dd2936e795
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-09 00:38:08 +0530

    Time: 11 ms (98.37%) | Memory: 18 MB (64.74%) - LeetSync

--- LeetCode/322-coin-change/coin-change.cpp
+++ LeetCode/322-coin-change/coin-change.cpp
@@ -1,25 +1,18 @@
-/*
-By    :: 
-Used  :: Memoization
-*/
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount){
-        vector<int>dp(amount+1,-2);
-        return helper(coins,amount,dp);
-    }
-    int helper(vector<int>&coins,int amt,vector<int>&dp)
-    {
-        if(amt==0)return 0;
-        if(amt<0)return -1;
-        if(dp[amt]!=-2)return dp[amt];
-        int res=INT_MAX;
-        for(int coin:coins)
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int>dp(amount+1,amount+1);
+        dp[0]=0;
+        for(int i=1;i<=amount;i++)
         {
-            int sub=helper(coins,amt-coin,dp);
-            if(sub==-1)continue;
-            res=min(res,sub+1);
+            for(int coin:coins)
+            {
+                if(i-coin>=0)
+                {
+                    dp[i]=min(dp[i],1+dp[i-coin]);
+                }
+            }
         }
-        return dp[amt]=(res==INT_MAX?-1:res);
+        return dp[amount]>amount?-1:dp[amount];
     }
 };
