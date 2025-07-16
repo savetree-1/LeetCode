6f5c26a167cfed7a179335f053d760a905c4bd10
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-17 01:15:14 +0530

    Time: 74 ms (79.08%) | Memory: 46.1 MB (72.65%) - LeetSync

--- LeetCode/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
+++ LeetCode/1706-min-cost-to-connect-all-points/min-cost-to-connect-all-points.cpp
@@ -1,57 +1,27 @@
 class Solution {
 public:
-    class DSU
-    {
-        public:
-        vector<int>parent,rank;
-        DSU(int n)
+    int minCostConnectPoints(vector<vector<int>>& points) {
+        int n=points.size(),cost=0,edges_used=0;
+        vector<bool>inMST(n,false);
+        priority_queue<pair<int,int>,vector<pair<int,int>>,greater<>>pq;
+        pq.push({0,0});
+        while(edges_used<n)
         {
-            parent.resize(n+1);
-            rank.resize(n+1,0);
-            for(int i=0;i<=n;i++)parent[i]=i;
-        }
-        int find(int u)
-        {
-            if(parent[u]==u)return u;
-            return parent[u]=find(parent[u]);
-        }
-        bool unite(int u,int v)
-        {
-            int pu=find(u);
-            int pv=find(v);
-            if(pu==pv)return false;
-            if(rank[pu]<rank[pv])parent[pu]=pv;
-            else if(rank[pv]<rank[pu])parent[pv]=pu;
-            else
+            auto[mincost,u]=pq.top();
+            pq.pop();
+            if(inMST[u])continue;
+            inMST[u]=true;
+            cost+=mincost;
+            edges_used++;
+            for(int v=0;v<n;v++)
             {
-                parent[pu]=pv;
-                rank[pv]++;
+                if(!inMST[v])
+                {
+                    int dist = abs(points[u][0] - points[v][0]) + abs(points[u][1] - points[v][1]);
+                    pq.push({dist,v});
+                }
             }
-            return true;
         }
-    };
-    int minCostConnectPoints(vector<vector<int>>& points){
-    int n=points.size();
-    vector<tuple<int,int,int>>edges;
-    for(int i=0;i<n;i++)
-    {
-        for(int j=i+1;j<n;j++)
-        {
-            int dist=abs(points[i][0]-points[j][0])+abs(points[i][1]-points[j][1]);
-            edges.push_back({dist,i,j});
-        }
-    }   
-    sort(edges.begin(),edges.end());
-    DSU dsu(n);
-    int total=0,edges_used=0;
-    for( auto& [cost,u,v]:edges)
-    {
-        if(dsu.unite(u,v))
-        {
-            total+=cost;
-            if(edges_used==n-1)break;
-        }
-    } 
-    return total;    
+        return cost;
     }
 };
