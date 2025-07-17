4897ed958b20ddd37da31da1bfd21c872b54d013
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-17 23:45:39 +0530

    Time: 81 ms (71.11%) | Memory: 44.3 MB (67.51%) - LeetSync

---
+++ LeetCode/744-network-delay-time/network-delay-time.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int networkDelayTime(vector<vector<int>>& times, int n, int k){
+        vector<vector<pair<int,int>>> graph(n+1);
+        for(auto& edge : times){
+            graph[edge[0]].push_back({edge[1], edge[2]});
+        }
+
+        vector<int> dist(n+1, INT_MAX);
+        dist[k] = 0;
+
+        priority_queue<pair<int,int>, vector<pair<int,int>>, greater<>> pq;
+        pq.push({0, k});
+
+        while(!pq.empty()){
+            int time = pq.top().first;
+            int node = pq.top().second;
+            pq.pop();
+
+            if(time > dist[node]) continue; // Important optimization
+
+            for(auto src : graph[node]){
+                if(time + src.second < dist[src.first]){
+                    dist[src.first] = time + src.second;
+                    pq.push({dist[src.first], src.first});
+                }
+            }
+        }
+
+        int maxi = *max_element(dist.begin() + 1, dist.end());
+        return (maxi == INT_MAX) ? -1 : maxi;
+    }
+};
