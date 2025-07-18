1e99d4a3ebb873d27a709958165bf771a3910ed3
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-19 01:29:46 +0530

    Time: 23 ms (79.76%) | Memory: 27.5 MB (28.46%) - LeetSync

--- LeetCode/1250-longest-common-subsequence/longest-common-subsequence.cpp
+++ LeetCode/1250-longest-common-subsequence/longest-common-subsequence.cpp
@@ -1,22 +1,18 @@
 class Solution {
 public:
-    vector<vector<int>> dp;
-
-    int solve(const string& text1, const string& text2, int i, int j) {
-        if (i == 0 || j == 0) return 0;
-        if (dp[i][j] != -1) return dp[i][j];
-
-        if (text1[i - 1] == text2[j - 1])
-            dp[i][j] = 1 + solve(text1, text2, i - 1, j - 1);
-        else
-            dp[i][j] = max(solve(text1, text2, i - 1, j), solve(text1, text2, i, j - 1));
-
-        return dp[i][j];
-    }
-
     int longestCommonSubsequence(string text1, string text2) {
-        int m = text1.size(), n = text2.size();
-        dp.assign(m + 1, vector<int>(n + 1, -1));
-        return solve(text1, text2, m, n);
+        int n=text1.size();
+        int m=text2.size();
+        vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=m;j++)
+            {
+                if(text1[i-1]==text2[j-1])dp[i][j]=1+dp[i-1][j-1];
+                else dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+        return dp[n][m];
+
     }
 };
