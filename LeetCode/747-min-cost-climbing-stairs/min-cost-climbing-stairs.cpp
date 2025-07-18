5b6dece01a1008b9c47d81a01186b8c75807ae0a
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-18 23:50:13 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (52.86%) - LeetSync

--- LeetCode/747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
+++ LeetCode/747-min-cost-climbing-stairs/min-cost-climbing-stairs.cpp
@@ -1,16 +1,16 @@
 class Solution {
 public:
-    int solve(vector<int>&cost,vector<int>&dp,int i)
-    {
-        if(i==0||i==1)return 0;
-        if(dp[i]!=-1)return dp[i];
-        return dp[i]= min(solve(cost,dp,i-1)+cost[i-1],solve(cost,dp,i-2)+cost[i-2]); 
-
-    }
-    int minCostClimbingStairs(vector<int>& cost)
-    {
-        int n=cost.size();
-        vector<int>dp(n+1,-1);
-        return solve(cost,dp,n);    
+    int minCostClimbingStairs(vector<int>& cost) {
+        int n=cost.size();        
+        vector<int>dp(n+1,0);
+        //dp[0]=cost[0];
+        //dp[1]=cost[1];
+        for(int i=2;i<=n;i++)
+        {
+            int left=dp[i-1]+cost[i-1];
+            int right=dp[i-2]+cost[i-2];
+            dp[i]=min(left,right);
+        }
+        return dp[n];
     }
 };
