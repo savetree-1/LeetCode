aae1e9d3f76d2c2bb17bda2ea1191fb97d541b2c
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-20 00:20:54 +0530

    Time: 0 ms (100.00%) | Memory: 10.2 MB (76.25%) - LeetSync

--- LeetCode/213-house-robber-ii/house-robber-ii.cpp
+++ LeetCode/213-house-robber-ii/house-robber-ii.cpp
@@ -1,24 +1,19 @@
 class Solution {
 public:
-    int func(vector<int>&nums,int i,int r){
-        int n=r-i+1;
-        if(n==0)return 0;
-        if(n==1)return nums[i];
-        vector<int>dp(n,0);
-        dp[0]=nums[i];
-        for(int j=1;j<n;j++){
-            int pick=nums[j+i];
-            if(j>1)pick+=dp[j-2];
-            int not_p=dp[j-1];
-            dp[j]=max(pick,not_p);
-        } 
-        return dp[n-1];
+    int robRange(vector<int>& nums, int l, int r) {
+        int prev=0, curr=0;
+        for(int i=l;i<=r;i++){
+            int temp=max(curr, prev+nums[i]);
+            prev=curr;
+            curr=temp;
+        }
+        return curr;
     }
 
     int rob(vector<int>& nums) {
         int n=nums.size();
         if(n==0)return 0;
         if(n==1)return nums[0];
-        return max(func(nums,0,n-2),func(nums,1,n-1));
+        return max(robRange(nums, 0, n-2), robRange(nums, 1, n-1));
     }
 };
