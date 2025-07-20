4b4f7621042b18fa6549212db0dd914e60ae85da
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-20 17:16:13 +0530

    [62] [Time Beats: 18.71%] [Memory Beats: 84.37%] - LeetPush

--- LeetCode/Unique-Paths.cpp
+++ LeetCode/Unique-Paths.cpp
@@ -1,19 +1,14 @@
 class Solution {
 public:
     int uniquePaths(int m, int n) {
-        vector<int>prev(n,0);
-        for(int i=0;i<m;i++)
+        vector<int>dp(n,1);
+        for(int i=1;i<m;i++)
         {
-            vector<int>curr(n,0);
-            for(int j=0;j<n;j++)
+            for(int j=1;j<n;j++)
             {
-                if(i==0|| j==0)curr[j]=1;
-                else{
-                if(i>0)curr[j]+=prev[j];
-                if(j>0)curr[j]+=curr[j-1];
-            }}
-            prev=curr;
+                dp[j]+=dp[j-1];
+            }
         }
-        return prev[n-1];
+        return dp[n-1];
     }
 };
