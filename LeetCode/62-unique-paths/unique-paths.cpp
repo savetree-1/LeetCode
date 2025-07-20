c0708e523071b78fd3d412d70fa9b6c129699faf
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-20 17:01:50 +0530

    Time: 0 ms (100.00%) | Memory: 9.3 MB (33.74%) - LeetSync

--- LeetCode/62-unique-paths/unique-paths.cpp
+++ LeetCode/62-unique-paths/unique-paths.cpp
@@ -7,8 +7,15 @@ public:
         if(dp[i][j]!=-1)return dp[i][j];
         return dp[i][j]=func(i+1,j,m,n,dp) +func(i,j+1,m,n,dp);
     }
-    int uniquePaths(int m, int n) {
-        vector<vector<int>>dp(m,vector<int>(n,-1));
-        return func(0,0,m,n,dp);
+   int uniquePaths(int m, int n) {
+    vector<vector<int>> dp(m, vector<int>(n, 0));
+    for(int i=0;i<m;i++) {
+        for(int j=0;j<n;j++) {
+            if(i==0 || j==0) dp[i][j] = 1;
+            else dp[i][j] = dp[i-1][j] + dp[i][j-1];
+        }
     }
+    return dp[m-1][n-1];
+}
+
 };
