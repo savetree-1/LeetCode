b9c0f2428912e95863e139434a669404e4fc0c10
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-21 18:35:22 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (76.34%) - LeetSync

--- LeetCode/64-minimum-path-sum/minimum-path-sum.cpp
+++ LeetCode/64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,29 +1,19 @@
 class Solution {
 public:
     int minPathSum(vector<vector<int>>& grid) {
-        int m=grid.size();
-        int n=grid[0].size();
-        vector<vector<int>>dp(m,vector<int>(n,0));
-        dp[0][0]=grid[0][0];
-        for(int i=0;i<m;i++)
-        {
-            for(int j=0;j<n;j++)
-            {
-                if(i==0 && j==0)dp[i][j]==grid[0][0];
-                else if(j==0)
-                {
-                    dp[i][j]=grid[i][j]+dp[i-1][j];
-                }
-                else if(i==0)
-                {
-                    dp[i][j]=grid[i][j]+dp[i][j-1];
-                }
-                else
-                {
-                    dp[i][j]=grid[i][j]+min(dp[i][j-1],dp[i-1][j]);
-                }
+        if(grid.empty()) return 0;
+        int m = grid.size(), n = grid[0].size();
+        vector<int> prev(n, 0);
+        for(int i = 0; i < m; i++) {
+            vector<int> curr(n, 0);
+            for(int j = 0; j < n; j++) {
+                if(i == 0 && j == 0) curr[j] = grid[i][j];
+                else if(i == 0) curr[j] = grid[i][j] + curr[j - 1]; // fix for first row
+                else if(j == 0) curr[j] = grid[i][j] + prev[j];     // first column
+                else curr[j] = grid[i][j] + min(curr[j - 1], prev[j]);
             }
+            prev = curr;
         }
-        return dp[m-1][n-1];
+        return prev[n - 1];
     }
 };
