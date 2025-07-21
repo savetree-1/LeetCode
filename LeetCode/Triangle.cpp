eff76de7c8cc6d7a05c8e5f8bdb17ed2081b3055
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-21 22:43:31 +0530

    [120] [Time Beats: 100.00%] [Memory Beats: 5.90%] - LeetPush

--- LeetCode/Triangle.cpp
+++ LeetCode/Triangle.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
-    int func(int i,int j,int m,vector<vector<int>>&tri,vector<vector<int>>&dp)
-    {
-        if(i==m-1)return tri[i][j];
-        if(dp[i][j]!=INT_MAX)return dp[i][j];
-        return dp[i][j]=tri[i][j]+min(func(i+1,j,m,tri,dp),func(i+1,j+1,m,tri,dp));
-    }
     int minimumTotal(vector<vector<int>>& tri) {
         int m=tri.size();
-        int n=tri[m-1].size();
-        vector<vector<int>>dp(m,vector<int>(n,INT_MAX));
-        return func(0,0,m,tri,dp);
+        vector<vector<int>>dp(m,vector<int>(m,INT_MAX));
+        for(int j=0;j<m;j++)
+        {
+            dp[m-1][j]=tri[m-1][j];
+        }
+        for(int i=m-2;i>=0;i--)
+        {
+            for(int j=i;j>=0;j--)
+            {
+                dp[i][j]=tri[i][j]+min(dp[i+1][j+1],dp[i+1][j]);
+            }
+        }
+        return dp[0][0];
     }
 };
