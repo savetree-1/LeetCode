12652a059173f3f06b86c611ba773f467db3803a
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-21 18:15:24 +0530

    Time: 0 ms (100.00%) | Memory: 16.3 MB (61.79%) - LeetSync

--- LeetCode/64-minimum-path-sum/minimum-path-sum.cpp
+++ LeetCode/64-minimum-path-sum/minimum-path-sum.cpp
@@ -1,16 +1,29 @@
 class Solution {
 public:
-    long long int func(int i,int j,int m,int n,vector<vector<int>>&grid,vector<vector<int>>&dp)
-    {
-        if(i==m-1&&j==n-1)return grid[i][j];
-        if(i>=m ||j>=n)return INT_MAX;
-        if(dp[i][j]!=0)return dp[i][j];
-        return dp[i][j]=min(grid[i][j]+func(i+1,j,m,n,grid,dp),grid[i][j]+func(i,j+1,m,n,grid,dp));
-    }
     int minPathSum(vector<vector<int>>& grid) {
         int m=grid.size();
         int n=grid[0].size();
         vector<vector<int>>dp(m,vector<int>(n,0));
-        return func(0,0,m,n,grid,dp);
+        dp[0][0]=grid[0][0];
+        for(int i=0;i<m;i++)
+        {
+            for(int j=0;j<n;j++)
+            {
+                if(i==0 && j==0)dp[i][j]==grid[0][0];
+                else if(j==0)
+                {
+                    dp[i][j]=grid[i][j]+dp[i-1][j];
+                }
+                else if(i==0)
+                {
+                    dp[i][j]=grid[i][j]+dp[i][j-1];
+                }
+                else
+                {
+                    dp[i][j]=grid[i][j]+min(dp[i][j-1],dp[i-1][j]);
+                }
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
