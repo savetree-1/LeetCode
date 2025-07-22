5462432b7419fff479905f2e70e5defc3d5af12d
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-22 13:36:49 +0530

    [931] [Time Beats: 32.56%] [Memory Beats: 5.79%] - LeetPush

---
+++ LeetCode/Minimum-Falling-Path-Sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int func(int i,int j,int n,vector<vector<int>>&matrix,vector<vector<int>>&dp)
+    {
+        if(j<0 || j>=n)return INT_MAX;
+        if(i==n-1)return matrix[i][j];
+        if(dp[i][j]!=-101)return dp[i][j];
+        int down=func(i+1,j,n,matrix,dp);
+        int diaright=func(i+1,j+1,n,matrix,dp);
+        int dialeft=func(i+1,j-1,n,matrix,dp);
+        return dp[i][j]=matrix[i][j]+min({down,diaright,dialeft});
+        }
+    int minFallingPathSum(vector<vector<int>>& matrix) {
+        int n=matrix.size();
+        int mini=INT_MAX;
+        vector<vector<int>>dp(n,vector<int>(n,-101));
+        for(int i=0;i<n;i++)
+        {
+            int res=func(0,i,n,matrix,dp);
+            mini=min(res,mini);
+        }
+        return mini;
+    }
+};
