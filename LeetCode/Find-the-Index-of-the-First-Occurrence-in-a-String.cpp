4a14355429d37136483cb1c05bce85ebd2be538e
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-27 19:36:41 +0530

    [28] [Time Beats: 100.00%] [Memory Beats: 14.64%] - LeetPush

--- LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
+++ LeetCode/Find-the-Index-of-the-First-Occurrence-in-a-String.cpp
@@ -1,15 +1,52 @@
 class Solution {
 public:
     int strStr(string h, string n) {
-        if(n.length()>h.length())return -1;
-        for(int i=0;i<h.length();i++)
+        vector<int>lps(n.size(),0);
+        lps[0]=0;
+        int i=1;
+        int len=0;
+        while(i<n.size())
         {
-            int j=0;
-            int dum=i;
-            while(h[dum]==n[j])
+            if(n[i]==n[len])
             {
-                dum++;j++;
-                if(j==n.length())return dum-n.length();
+                len++;
+                lps[i]=len;
+                i++;
+            }
+            else
+            {
+                if(len!=0)
+                {
+                    len=lps[len-1];
+                }
+                else
+                {
+                    lps[i]=0;
+                    i++;
+                }
+            }
+        }
+        i=0;int j=0;
+        while(i<h.size())
+        {
+            if(h[i]==n[j])
+            {
+                i++;j++;
+            }
+            if(j==n.size())
+            {
+                return i-j;
+            }
+            else if(i<h.size() && h[i]!=n[j])
+            {
+                if(j!=0)
+                {
+                    j=lps[j-1];
+                }
+                else
+                {
+                    i++;
+                }
             }
         }
         return -1;
