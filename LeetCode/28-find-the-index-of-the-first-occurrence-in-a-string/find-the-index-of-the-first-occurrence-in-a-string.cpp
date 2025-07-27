e633c6171fccd0bc7589164c89420b86f6a82512
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-27 18:18:23 +0530

    Time: 0 ms (100.00%) | Memory: 8.5 MB (96.08%) - LeetSync

--- LeetCode/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
+++ LeetCode/28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.cpp
@@ -1,17 +1,8 @@
 class Solution {
 public:
-    int strStr(string h, string n) {
-        if(n.length()>h.length())return -1;
-        for(int i=0;i<h.length();i++)
-        {
-            int j=0;
-            int dum=i;
-            while(h[dum]==n[j])
-            {
-                dum++;j++;
-                if(j==n.length())return dum-n.length();
-            }
-        }
+    int strStr(string haystack, string needle) {
+        size_t pos=haystack.find(needle);
+        if(pos!=string::npos)return pos;
         return -1;
     }
 };
