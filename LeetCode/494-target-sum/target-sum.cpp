fc4ceb43eeee258920fa651dc17dd02ec9708252
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-28 22:31:45 +0530

    Time: 0 ms (100.00%) | Memory: 12.2 MB (72.97%) - LeetSync

--- LeetCode/494-target-sum/target-sum.cpp
+++ LeetCode/494-target-sum/target-sum.cpp
@@ -6,22 +6,24 @@ typedef vector<int> vi;
 typedef vector<vi> vvi;
 class Solution {
 public:
-    int findTargetSumWays(vi& nums, int target) {
-        int total=accumulate(nums.begin(),nums.end(),0);
-        if((target+total)%2!=0||abs(target) > total) return 0;
-        int subsetSum=(target+total)/2;
-        return func(nums,subsetSum);
-    }
-    int func(vi &arr, int target) {
-        vi dp(target+1,0);
-        dp[0]=1;
-        for(int num:arr)
-        {
-            for(int j=target;j>=num;j--)
-            {
-                dp[j]+=dp[j-num];
-            }
+    int findTargetSumWays(vector<int>& nums, int target) {
+    int total = 0;
+    for (int x : nums) total += x;
+
+    if ((target + total) % 2 != 0 || target > total || target < -total) return 0;
+
+    int subsetSum = (target + total) / 2;
+
+    vector<int> dp(subsetSum + 1, 0);
+    dp[0] = 1;
+
+    for (int num : nums) {
+        for (int s = subsetSum; s >= num; --s) {
+            dp[s] += dp[s - num];
         }
-        return dp[target];
     }
+
+    return dp[subsetSum];
+}
+
 };
