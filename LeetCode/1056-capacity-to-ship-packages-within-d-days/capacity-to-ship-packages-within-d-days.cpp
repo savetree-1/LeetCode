7c5257cbf7ccde4f3edfc1f9a7699a67a5cacfde
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-28 23:05:19 +0530

    Time: 12 ms (54.50%) | Memory: 35 MB (34.78%) - LeetSync

--- LeetCode/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
+++ LeetCode/1056-capacity-to-ship-packages-within-d-days/capacity-to-ship-packages-within-d-days.cpp
@@ -1,29 +1,25 @@
 class Solution {
 public:
-    bool check(vector<int>&weights,int days,int maxi)
-    {
-        int d=1;
-        int sum=0;
-        for(int num:weights)
-        {
-            if(num>maxi)return false;
-            if(sum+num>maxi){d++;sum=num;}
-            else sum+=num;
+    bool isPossible(vector<int>& weights, int days, int cap) {
+        int cur = 0, d = 1;
+        for(int wt : weights) {
+            if(cur + wt > cap) {
+                d++;
+                cur = 0;
+            }
+            cur += wt;
         }
-        return d<=days;
+        return d <= days;
     }
+
     int shipWithinDays(vector<int>& weights, int days) {
-       int low=1;int high=accumulate(weights.begin(),weights.end(),0);
-       int ans=0;
-       while(low<=high)
-       {
-        int mid=low+(high-low)/2;
-        if(check(weights,days,mid)){
-            ans=mid;
-            high=mid-1;
+        int low = *max_element(weights.begin(), weights.end());
+        int high = accumulate(weights.begin(), weights.end(), 0);
+        while(low <= high) { 
+            int mid = (low + high) / 2;
+            if(isPossible(weights, days, mid)) high = mid-1; 
+            else low = mid + 1;
         }
-        else low=mid+1;
-       } 
-       return ans;
+        return low;
     }
 };
