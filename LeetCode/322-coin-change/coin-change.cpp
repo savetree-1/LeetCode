e18231618527754e7d7faa4a96e8ebbd3ae3882b
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-28 08:54:53 +0530

    Time: 19 ms (84.01%) | Memory: 18 MB (75.12%) - LeetSync

--- LeetCode/322-coin-change/coin-change.cpp
+++ LeetCode/322-coin-change/coin-change.cpp
@@ -1,18 +1,19 @@
 class Solution {
 public:
-    int coinChange(vector<int>& coins, int amount) {
-        vector<int>dp(amount+1,amount+1);
+    int coinChange(vector<int>& coins, int amt){
+        vector<int>dp(amt+1,amt+1);
         dp[0]=0;
-        for(int i=1;i<=amount;i++)
+        for(int i=1;i<=amt;i++)
         {
             for(int coin:coins)
             {
                 if(i-coin>=0)
                 {
-                    dp[i]=min(dp[i],1+dp[i-coin]);
+                    dp[i]=min(1+dp[i-coin],dp[i]);
                 }
             }
         }
-        return dp[amount]>amount?-1:dp[amount];
+        if(dp[amt]>amt)return -1;
+        return dp[amt];
     }
 };
