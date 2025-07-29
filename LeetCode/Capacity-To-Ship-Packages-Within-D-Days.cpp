834a74d7ec5ecfa807a86e24992090767337ad47
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-29 06:12:11 +0530

    [1011] [Time Beats: 68.02%] [Memory Beats: 8.67%] - LeetPush

--- LeetCode/Capacity-To-Ship-Packages-Within-D-Days.cpp
+++ LeetCode/Capacity-To-Ship-Packages-Within-D-Days.cpp
@@ -1,34 +1,38 @@
-/*
-By   :: savetrees
-Used :: Binary Search
-*/
 class Solution {
 public:
-    int isValid(int check,vector<int>& weights,int days) {
-        int count=1,pre=0;
-        for (int num : weights) {
-            pre+=num;
-            if (pre>check) {
-                count++;
-                pre=num;
+     bool check(vector<int>&weights,int x,int days)
+        {
+            int tot=1;
+            //int i=0;
+            int prev=weights[0];
+            for(int i=1;i<weights.size();i++)
+            {
+                if(weights[i]+prev<=x)
+                {
+                    prev+=weights[i];
+                }
+                else {prev=weights[i];
+                    tot++;}
+            }
+            return tot<=days;
+        }
+    int shipWithinDays(vector<int>& weights, int days) {
+        int low=*max_element(weights.begin(),weights.end());
+        int high=accumulate(weights.begin(),weights.end(),0);
+        int ans;
+        while(low<=high)
+        {
+            int mid=low+(high-low)/2;
+            if(check(weights,mid,days))
+            {
+                ans=mid;
+                high=mid-1;
+            }
+            else
+            {
+                low=mid+1;
             }
         }
-        return count;
-    }
-    
-    int shipWithinDays(vector<int>& weights,int days) {
-        int maxi=0,sum=0;
-        for (int num:weights) {
-            maxi=max(maxi,num);
-            sum+=num;
-        }
-        int low=maxi,high=sum;
-        while (low<=high) {
-            int mid=low+(high-low)/2;
-            int dummy=isValid(mid,weights,days);
-            if (dummy>days)low=mid+1;
-            else high=mid-1;
-        }
-        return low;
+        return ans;
     }
 };
