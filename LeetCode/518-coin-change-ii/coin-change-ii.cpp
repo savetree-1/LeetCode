6b078ef6aa3a6d20671ff204f889560f40e21132
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-30 12:43:47 +0530

    Time: 27 ms (36.20%) | Memory: 46.3 MB (11.58%) - LeetSync

--- LeetCode/518-coin-change-ii/coin-change-ii.cpp
+++ LeetCode/518-coin-change-ii/coin-change-ii.cpp
@@ -1,18 +1,21 @@
 class Solution {
 public:
-    int change(int amt,vector<int>& coins ){
-        vector<unsigned long long>dp(amt+1,0);
-        dp[0]=1;
-        for(int coin:coins){
-        for(int i=1;i<=amt;i++)
-        {
-            
-                if(i-coin>=0)
-                {
-                    dp[i]+=dp[i-coin];
-                }
+    int change(int amt, vector<int>& coins) {
+        vector<vector<unsigned long long>> dp(coins.size(), vector<unsigned long long>(amt + 1, 0));
+
+        for(int i = 0; i <= amt; i++) {
+            if(i % coins[0] == 0) dp[0][i] = 1;
+        }
+
+        for(int i = 1; i < coins.size(); i++) {
+            for(int j = 0; j <= amt; j++) {
+                unsigned long long nottake = dp[i - 1][j];
+                unsigned long long take = 0;
+                if(j >= coins[i]) take = dp[i][j - coins[i]];
+                dp[i][j] = take + nottake;
             }
         }
-        return dp[amt];
+
+        return dp[coins.size() - 1][amt];
     }
 };
