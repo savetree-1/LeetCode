d6be57fde7abcf4328768a73a5c110ec4c6523d3
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-07-30 11:28:12 +0530

    Time: 3 ms (95.28%) | Memory: 10.3 MB (87.20%) - LeetSync

--- LeetCode/518-coin-change-ii/coin-change-ii.cpp
+++ LeetCode/518-coin-change-ii/coin-change-ii.cpp
@@ -1,14 +1,18 @@
 class Solution {
 public:
-    int change(int amount, vector<int>& coins) {
-        const int MOD = 1e9 + 7; // Use modulo to prevent overflow
-        vector<unsigned long long> dp(amount + 1, 0);
-        dp[0] = 1;
-        for(int coin : coins) {
-            for(int i = coin; i <= amount; i++) {
-                dp[i] = (dp[i] + dp[i - coin]);
+    int change(int amt,vector<int>& coins ){
+        vector<unsigned long long>dp(amt+1,0);
+        dp[0]=1;
+        for(int coin:coins){
+        for(int i=1;i<=amt;i++)
+        {
+            
+                if(i-coin>=0)
+                {
+                    dp[i]+=dp[i-coin];
+                }
             }
         }
-        return dp[amount];
+        return dp[amt];
     }
 };
