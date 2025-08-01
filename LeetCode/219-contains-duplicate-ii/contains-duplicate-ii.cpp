646ccaea1cc36909371a30ef6d723aa3d0f89970
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-08-02 00:25:23 +0530

    Time: 71 ms (76.75%) | Memory: 98.8 MB (15.36%) - LeetSync

--- LeetCode/219-contains-duplicate-ii/contains-duplicate-ii.cpp
+++ LeetCode/219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -1,15 +1,11 @@
 class Solution {
 public:
     bool containsNearbyDuplicate(vector<int>& nums, int k) {
-        int n=nums.size();
-        if(k>=n)k=n-1;
-        for(int i=0;i<n-1;i++)
+        unordered_map<int,int>mpp;
+        for(int i=0;i<nums.size();i++)
         {
-            if(i+k>=n)k=n-i-1;
-            for(int temp=1;temp<=k;temp++)
-            {
-                if(nums[i]==nums[i+temp])return true;
-            }
+            if(mpp.count(nums[i]) && i-mpp[nums[i]]<=k)return true;
+            mpp[nums[i]]=i;
         }
         return false;
     }
