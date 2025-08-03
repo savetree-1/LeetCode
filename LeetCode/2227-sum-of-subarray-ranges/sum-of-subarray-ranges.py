9dcc6df35df008fb7aa69ac6b803570c7f74e084
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-08-03 11:01:05 +0530

    Time: 13 ms (87.72%) | Memory: 17.9 MB (69.39%) - LeetSync

--- LeetCode/2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
+++ LeetCode/2227-sum-of-subarray-ranges/sum-of-subarray-ranges.py
@@ -1,21 +1,28 @@
+LMT = 10**5
+INF = 10**9
+
 class Solution:
-    def subArrayRanges(self, nums: List[int]) -> int:
-        def sumRange(idx: int, minVal: int, maxVal: int) -> int:
-            total = maxVal - minVal
+    def subArrayRanges(self, arr: list[int]) -> int:
+        n = len(arr)
 
-            if(idx >= len(nums)):
-                return total
+        gt = lambda: []
+        stk, res = gt(), 0
 
-            minVal = nums[idx] if nums[idx] < minVal else minVal
-            maxVal = nums[idx] if nums[idx] > maxVal else maxVal
-            total += sumRange(idx+1, minVal, maxVal)
+        # max contribution
+        for i in range(n + 1):
+            while stk and (i == n or arr[stk[-1]] < (arr[i] if i < n else -INF)):
+                j = stk.pop()
+                l = stk[-1] if stk else -1
+                res += (i - j) * (j - l) * arr[j]
+            stk.append(i)
 
-            return total
+        stk = gt()
+        # min contribution
+        for i in range(n + 1):
+            while stk and (i == n or arr[stk[-1]] > (arr[i] if i < n else INF)):
+                j = stk.pop()
+                l = stk[-1] if stk else -1
+                res -= (i - j) * (j - l) * arr[j]
+            stk.append(i)
 
-        ans = 0
-        for idx, num in enumerate(nums):
-            if (idx > 0 and nums[idx] == nums[idx-1]):
-                continue
-            ans += sumRange(idx, num, num)
-
-        return ans
+        return res
