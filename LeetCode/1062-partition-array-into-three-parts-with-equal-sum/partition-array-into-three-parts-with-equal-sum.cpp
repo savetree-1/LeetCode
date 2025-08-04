ec3477f31a7e38984d1ee123acf98cf3b09fa1e1
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-08-04 08:38:39 +0530

    Time: 2 ms (16.49%) | Memory: 39.2 MB (5.32%) - LeetSync

--- LeetCode/1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.cpp
+++ LeetCode/1062-partition-array-into-three-parts-with-equal-sum/partition-array-into-three-parts-with-equal-sum.cpp
@@ -8,10 +8,13 @@ public:
         vector<int>forw;
         int sum1=arr[0];
         forw.push_back(sum1);
+        int count;
         for(int i=1;i<n;i++) {
             sum1+=arr[i];
             forw.push_back(sum1);
+            if(arr[i]==0)count++;
         }
+        if(count==arr.size()-1)return true;
         for(int i=0;i<n-2;i++) {
             if(forw[i]==target) {
                 for(int j=i+1;j<n-1;j++) {
