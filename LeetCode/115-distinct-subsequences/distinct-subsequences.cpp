f1e1efbffc7b6f839be31f8a9eff450e455a76e9
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-08-23 23:19:50 +0530

    Time: 8 ms (92.86%) | Memory: 9 MB (91.34%) - LeetSync

---
+++ LeetCode/115-distinct-subsequences/distinct-subsequences.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    int numDistinct(string s, string t) {
+    int n = s.size(), m = t.size();
+    vector<double> prev(m + 1, 0), cur(m + 1, 0);
+
+    prev[0] = 1;
+
+    for (int i = 1; i <= n; i++) {
+        cur[0] = 1;
+        for (int j = 1; j <= m; j++) {
+            if (s[i - 1] == t[j - 1])
+                cur[j] = prev[j - 1] + prev[j];
+            else
+                cur[j] = prev[j];
+        }
+        prev = cur;
+    }
+    return (int)prev[m];
+}
+
+};
