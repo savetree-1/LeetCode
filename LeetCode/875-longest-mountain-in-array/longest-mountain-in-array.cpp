a22c3faeaadce1bccdd023b80e865252c5d8350d
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2025-09-22 16:38:51 +0530

    Time: 0 ms (100.00%) | Memory: 22.3 MB (53.69%) - LeetSync

---
+++ LeetCode/875-longest-mountain-in-array/longest-mountain-in-array.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int longestMountain(vector<int>& arr) {
+        int n = arr.size();
+        if(n < 3) return 0;
+        int ans = 0;
+        int low = 1;
+        while(low < n - 1) {
+            if(arr[low-1] < arr[low] && arr[low] > arr[low+1]) {
+                int l = low, r = low;
+                while(l > 0 && arr[l-1] < arr[l]) l--;
+                while(r < n-1 && arr[r] > arr[r+1]) r++;
+                ans = max(ans, r - l + 1);
+                low = r; 
+            } else {
+                low++;
+            }
+        }
+        return ans;
+    }
+};
