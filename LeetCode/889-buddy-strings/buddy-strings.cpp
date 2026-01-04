2d23f710aa4847f059d8871a9d6e3eb34613c833
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-01-04 22:34:42 +0530

    Time: 2 ms (10.54%) | Memory: 9.3 MB (35.20%) - LeetSync

--- LeetCode/889-buddy-strings/buddy-strings.cpp
+++ LeetCode/889-buddy-strings/buddy-strings.cpp
@@ -4,14 +4,11 @@ public:
         if (s.length() != goal.length()) return false;
         if (s == goal) {
             unordered_set<char> st(s.begin(), s.end());
-            return st.size() < s.length();
-        }
+            return st.size() < s.length();}
         vector<int> diff;
         for (int i = 0; i < s.length(); i++) {
             if (s[i] != goal[i]) {
-                diff.push_back(i);
-            }
-        }
+                diff.push_back(i);}}
         return diff.size() == 2 && s[diff[0]] == goal[diff[1]] && s[diff[1]] == goal[diff[0]];
     }
 };
