0a2f5a02d3a297042cf2e9ac856abf12110f72bc
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-01-07 06:31:38 +0530

    Time: 4 ms (65.02%) | Memory: 78.1 MB (73.71%) - LeetSync

---
+++ LeetCode/1465-maximum-product-of-splitted-binary-tree/maximum-product-of-splitted-binary-tree.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    long long mod = 1e9 + 7;
+    long long totalSum = 0;
+    long long maxProduc = 0;
+    long long dfsSum(TreeNode* root) {
+        if (!root) return 0;
+        return root->val + dfsSum(root->left) + dfsSum(root->right);
+    }
+
+    long long dfsProduct(TreeNode* root) {
+        if (!root) return 0;
+        long long left = dfsProduct(root->left);
+        long long right = dfsProduct(root->right);
+        long long subSum = root->val + left + right;
+        maxProduc = max(maxProduc, subSum * (totalSum - subSum));
+        return subSum;
+    }
+    int maxProduct(TreeNode* root) {
+        totalSum = dfsSum(root);     
+        dfsProduct(root);            
+        return maxProduc % mod;
+    }
+};
