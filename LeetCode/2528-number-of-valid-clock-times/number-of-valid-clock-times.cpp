9e1dbba09d830f506b7225f430e13f105a0d9032
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-01-08 15:19:41 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (26.19%) - LeetSync

--- LeetCode/2528-number-of-valid-clock-times/number-of-valid-clock-times.cpp
+++ LeetCode/2528-number-of-valid-clock-times/number-of-valid-clock-times.cpp
@@ -1,43 +1,21 @@
 class Solution {
 public:
     int countTime(string time) {
-        int x=1;
-        int y=1;
-        if(time[0]=='?' && time[1]=='?')x=24;
-        else if(time[0]=='?')
-        {
-            if(time[1]<'4' && time[1]>='0')
-            {
-                x=3;
-            }
-            else if(time[1]>='4')
-            {
-                x=2;
-            }
+        int x = 1, y = 1;
+        if (time[0] == '?' && time[1] == '?') {
+            x = 24;
+        } else if (time[0] == '?') {
+            x = (time[1] <= '3') ? 3 : 2;
+        } else if (time[1] == '?') {
+            x = (time[0] == '2') ? 4 : 10;
         }
-        else if(time[1]=='?')
-        {
-            if(time[0]<'2')
-            {
-                x*=10;
-            }
-            else
-            {
-                x*=4;
-            }
+        if (time[3] == '?' && time[4] == '?') {
+            y = 60;
+        } else if (time[3] == '?') {
+            y = 6;
+        } else if (time[4] == '?') {
+            y = 10;
         }
-        if(time[3]=='?' && time[4]=='?')
-        {
-            y=60;
-        }
-        else if(time[3]=='?')
-        {
-            y*=6;
-        } 
-        else if(time[4]=='?')
-        {
-            y*=10;
-        }
-        return x*y;
+        return x * y;
     }
 };
