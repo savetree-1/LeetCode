e07ccc6369fec8d30f730dc59d7ea9a87ba525ea
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-02-05 07:54:31 +0530

    Time: 8 ms (46.52%) | Memory: 25.8 MB (39.55%) - LeetSync

--- LeetCode/3651-transformed-array/transformed-array.cpp
+++ LeetCode/3651-transformed-array/transformed-array.cpp
@@ -3,21 +3,10 @@ public:
     vector<int> constructTransformedArray(vector<int>& nums) {
         int n = nums.size();
         vector<int> res(n);
-
-        for(int i = 0; i < n; i++)
-        {
-            if(nums[i] > 0)
-            {
-                res[i] = nums[(i + nums[i]) % n];
-            }
-            else if(nums[i] < 0)
-            {
-                res[i] = nums[(i + nums[i] % n + n) % n];
-            }
-            else
-            {
-                res[i] = nums[i];
-            }
+        for(int i = 0; i < n; i++) {
+            int idx = (i + nums[i]) % n;
+            if(idx < 0) idx += n;
+            res[i] = nums[idx];
         }
         return res;
     }
