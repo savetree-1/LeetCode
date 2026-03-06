0fa0aa089324faa947614bce602e03c9e247b97f
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-03-06 13:16:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.4 MB (10.47%) - LeetSync

--- LeetCode/1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
+++ LeetCode/1910-check-if-binary-string-has-at-most-one-segment-of-ones/check-if-binary-string-has-at-most-one-segment-of-ones.cpp
@@ -1,15 +1,13 @@
 class Solution {
 public:
     bool checkOnesSegment(string s) {
-        int count=0;
-        if(s[0]=='1')count++;
-        for(int i=1;i<s.length();i++)
-        {
-            if(s.length()>1 && s[i]=='1'&&s[i-1]=='0')
-            {
-                count++;
-            }
-        }
-        return count==1 || count==0;
+    int count=0;
+        if(s.length()==1)return s[0]=='1';
+        if(s[0]=='1')count=1;
+      for(int i=1;i<s.length();i++)
+      {
+        if(s[i]=='1' && s[i-1]!='1')count++;
+      }  
+      return count==1;
     }
 };
