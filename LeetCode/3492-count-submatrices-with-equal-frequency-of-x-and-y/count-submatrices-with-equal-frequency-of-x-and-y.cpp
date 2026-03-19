fdbff33171bd00ae1fe9644af31c4975f39c4972
Ankush Rawat <143923713+savetree-1@users.noreply.github.com>
2026-03-19 08:27:38 +0530

    Time: 63 ms (47.97%) | Memory: 129.3 MB (72.36%) - LeetSync

--- LeetCode/3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
+++ LeetCode/3492-count-submatrices-with-equal-frequency-of-x-and-y/count-submatrices-with-equal-frequency-of-x-and-y.cpp
@@ -3,38 +3,35 @@ public:
     int numberOfSubmatrices(vector<vector<char>>& grid) {
         int n = grid.size();
         int m = grid[0].size();
-        vector<vector<int>> sum(n, vector<int>(m, 0));
-        vector<vector<int>> cnt(n, vector<int>(m, 0));
+
+        vector<vector<pair<int,int>>> pre(n, vector<pair<int,int>>(m, {0,0}));
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
 
-                int val = 0;
-                if(grid[i][j] == 'X') val = 1;
-                else if(grid[i][j] == 'Y') val = -1;
+                if(grid[i][j] == 'X') pre[i][j].first++;
+                else if(grid[i][j] == 'Y') pre[i][j].second++;
 
-                sum[i][j] = val;
-                cnt[i][j] = (grid[i][j] == 'X');
-
-                if(i > 0) {
-                    sum[i][j] += sum[i-1][j];
-                    cnt[i][j] += cnt[i-1][j];
-                }
                 if(j > 0) {
-                    sum[i][j] += sum[i][j-1];
-                    cnt[i][j] += cnt[i][j-1];
-                }
-                if(i > 0 && j > 0) {
-                    sum[i][j] -= sum[i-1][j-1];
-                    cnt[i][j] -= cnt[i-1][j-1];
+                    pre[i][j].first += pre[i][j-1].first;
+                    pre[i][j].second += pre[i][j-1].second;
                 }
             }
         }
+
+        for(int j = 0; j < m; j++) {
+            for(int i = 1; i < n; i++) {
+                pre[i][j].first += pre[i-1][j].first;
+                pre[i][j].second += pre[i-1][j].second;
+            }
+        }
+
         int ans = 0;
+
         for(int i = 0; i < n; i++) {
             for(int j = 0; j < m; j++) {
-                if(sum[i][j] == 0 && cnt[i][j] > 0) {
+                if(pre[i][j].first > 0 && pre[i][j].first == pre[i][j].second)
                     ans++;
-                }
             }
         }
 
